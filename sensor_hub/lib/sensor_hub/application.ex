33c0a5dc394fbcd80643dfcd9e72beda15d8929f
ryan <ryanyogan@gmail.com>
2021-09-27 22:04:24 -0500

    start the sensors when on the target

--- sensor_hub/lib/sensor_hub/application.ex
+++ sensor_hub/lib/sensor_hub/application.ex
@@ -7,34 +7,21 @@ defmodule SensorHub.Application do
 
   @impl true
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SensorHub.Supervisor]
 
-    children =
-      [
-        # Children for all targets
-        # Starts a worker by calling: SensorHub.Worker.start_link(arg)
-        # {SensorHub.Worker, arg},
-      ] ++ children(target())
+    children = children(target())
 
     Supervisor.start_link(children, opts)
   end
 
-  # List all child processes to be supervised
   def children(:host) do
-    [
-      # Children that only run on the host
-      # Starts a worker by calling: SensorHub.Worker.start_link(arg)
-      # {SensorHub.Worker, arg},
-    ]
+    []
   end
 
   def children(_target) do
     [
-      # Children for all targets except host
-      # Starts a worker by calling: SensorHub.Worker.start_link(arg)
-      # {SensorHub.Worker, arg},
+      {BMP280, [i2c_address: 0x77, name: BMP280]},
+      {VEML6030, %{}}
     ]
   end
 
